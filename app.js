9099cec05abfbd9cc5d49eb6c6aa6271b982f312
Shahd-AlDosari <92652867+Shahd-AlDosari@users.noreply.github.com>
2021-10-18 22:47:13 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+console.log("This is working");
+
+(function () {
+    var myConnector = tableau.makeConnector();
+
+    myConnector.getSchema = function (schemaCallback) { 
+    	const covidCols = [
+      {
+        id: "Date_of_report",
+        dataType: tableau.dataTypeEnum.date,
+      },
+      {
+        id: "Municipality_code",
+        dataType: tableau.dataTypeEnum.string,
+      },
+      {
+        id: "Municipality_name",
+        dataType: tableau.dataTypeEnum.string,
+      },
+      {
+        id: "Province",
+        dataType: tableau.dataTypeEnum.string,
+      },
+      {
+        id: "Total_reported",
+        dataType: tableau.dataTypeEnum.int,
+      },
+      {
+        id: "Hospital_admission",
+        dataType: tableau.dataTypeEnum.int,
+      },
+      {
+        id: "Deceased",
+        dataType: tableau.dataTypeEnum.int,
+      },
+    ];
+
+    let covidTableSchema = {
+      id: "RIVM",
+      alias: "Dutch Corona Cases since start",
+      columns: covidCols,
+    };
+
+    schemaCallback([covidTableSchema])
+};
+
+    myConnector.getData = function (table, doneCallback) {
+
+    };
+
+    tableau.registerConnector(myConnector);
+})();
+
+document.querySelector("#getData").addEventListner('click', getData)
+function getData (){
+	tableau.connectionName = "Dutch Corona Numbers";
+	tableau.submit()
+}
